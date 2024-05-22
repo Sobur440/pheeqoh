1466825c26f5bbb1e13a4c4d50a9dc3a5be81421
sobur440 <abubakarsobur440@gmail.com>
2024-05-22 12:25:43 +0100

    add featured works section to the homepage

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="font-neueMontreal">
+      <body className="font-neueMontreal bg-[#ECE8DC]">
         <Navbar />
         {children}
         <Footer />
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./hero.module.css";
+import styles from "./home.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,7 +11,7 @@ const Home = () => {
       <section className={`${styles.hero} text-white w-full mx-auto pb-3`}>
         <div className="w-[95%] mx-auto h-full flex flex-col justify-end">
           <div className="w-full h-[55%] md:h-[70%] flex flex-col justify-between items-center mx-auto">
-            <div className="w-full font-ppEiko text-[17vw] md:text-[17vw] lg:text-[10vw] leading-[.8] lg:leading-none flex flex-col items-center">
+            <div className="w-full font-ppEiko text-[17vw] md:text-[17vw] lg:text-[10vw] leading-[.8] lg:leading-none flex flex-col items-center ml-[1.3rem] md:ml-[3.6rem] lg:ml-0">
               <div className="flex w-full mx-auto lg:justify-between">
                 <p className="uppercase">
                   be<span className="italic">l</span>lo
@@ -95,23 +95,94 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="flex flex-col lg:flex-row items-center md:items-end lg:justify-end gap-2 lg:gap-7 mt-[2rem] w-full mx-auto px-2">
+        <div className="flex flex-col lg:flex-row items-end lg:justify-end gap-2 lg:gap-7 mt-[2rem] w-full mx-auto px-2">
           <div className="lg:order-2">
             <Image
               src="/images/pheeqohfan.png"
               alt="taofeeqoh image holding a fan"
               width={500}
               height={500}
+              className="w-[30rem]"
             />
           </div>
           <Link
             href="/about"
-            className="uppercase self-end text-[1.5rem] font-bold lg:order-1"
+            className="uppercase self-end text-[1.5rem] font-extrabold lg:order-1"
           >
             know more
           </Link>
         </div>
       </section>
+
+      {/* FEATURED WORKS SECTION */}
+
+      <section className="mt-7 text-white w-full flex flex-col">
+        <p className="text-black uppercase font-extrabold ml-3 mb-2 text-[.8rem]">
+          featured works
+        </p>
+        <div
+          className={`w-full h-[20rem] md:h-[30rem] lg:h-screen relative flex items-end lg:items-center ${styles.pos}`}
+        >
+          <div className="w-[90%] mx-auto flex justify-between mb-4 lg:mb-0">
+            <p className="font-extrabold text-[1.2rem]">SeamlessPOS</p>
+            <p className="font-extrabold text-[1.2rem]">UI/UX DESIGN</p>
+            <p className="hidden lg:block max-w-[25rem] text-justify text-[.9rem]">
+              Meet SeamlessPOS, a game-changer for restaurant and hospitality
+              operations. This cutting-edge solution revolutionizes order and
+              inventory management, providing a seamless experience. With a
+              user-friendly design, SeamlessPOS empowers teams to handle orders,
+              manage inventory, and deliver outstanding service in real-time.
+            </p>
+          </div>
+        </div>
+
+        <div
+          className={`w-full h-[20rem] md:h-[30rem] lg:h-screen relative flex items-end lg:items-center my-4 lg:my-0 ${styles.eohs}`}
+        >
+          <div className="w-[90%] mx-auto flex justify-between mb-4 lg:mb-0">
+            <p className="font-extrabold text-[1.2rem]">EOHS</p>
+            <p className="font-extrabold text-[1.2rem]">UI/UX DESIGN</p>
+            <p className="hidden lg:block max-w-[25rem] text-justify text-[.9rem]">
+              I created this App to help customers of any Online Shopping
+              Platform to order and pay with their cryptocurrency with ease
+              especially E-commerce products within Web2 to learn a little about
+              Web3 because most people now trade with coins. Introducing this
+              feature into the Web2 products will make the ordering process
+              smooth and quick without the exchange of coins before any users
+              can get stuff online.
+            </p>
+          </div>
+        </div>
+
+        <div
+          className={`w-full h-[20rem] md:h-[30rem] lg:h-screen relative flex items-end lg:items-center ${styles.insta}`}
+        >
+          <div className="w-[90%] mx-auto flex justify-between mb-4 lg:mb-0">
+            <p className="font-extrabold text-[1.2rem] lg:max-w-[7rem]">
+              Instagram Mobile App Redesign
+            </p>
+            <p className="font-extrabold text-[1.2rem]">UI/UX DESIGN</p>
+            <p className="hidden lg:block max-w-[25rem] text-justify text-[.9rem]">
+              Enhancing accessibility by allowing users to effortlessly drag and
+              drop their preferred highlights without disruption. Introducing a
+              seamless dropdown above the highlights, providing easy navigation
+              through lists. Crafting an intuitive and interactive interface for
+              a simplified user experience.
+            </p>
+          </div>
+        </div>
+
+        <Link
+          href="/work"
+          className="block uppercase text-black font-extrabold text-[3rem] leading-[1em] self-center mt-10"
+        >
+          view all <span className="block ml-2">works</span>
+        </Link>
+      </section>
+
+      {/* REACHOUT */}
+
+      <section></section>
     </>
   );
 };
